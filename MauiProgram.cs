eefdceff24883c96fcb8572ab90ab6376d2fff61
inaki <iakitrapiella27@hotmail.com>
2025-11-20 13:06:22 -0300

    agrego DI para servicios, VM y Views

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using AppParcialesMauiTrapiella.Services;
+using AppParcialesMauiTrapiella.ViewsModels;
+using Microsoft.Extensions.Logging;
 
 namespace AppParcialesMauiTrapiella
 {
@@ -14,9 +16,16 @@ namespace AppParcialesMauiTrapiella
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+            builder.Services.AddSingleton<IValidacionServicio, ValidacionServicio>();
+
+            // Registro de ViewModels
+            builder.Services.AddTransient<MainPageViewModels>();
+
+            // Registro de paginas
+            builder.Services.AddTransient<MainPage>();
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
