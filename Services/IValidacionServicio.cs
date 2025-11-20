eea03d55cf44ddf81006624c1b2b0e60ca4e67b2
inaki <iakitrapiella27@hotmail.com>
2025-11-20 13:04:29 -0300

    agrego servicios de validacion

---
+++ Services/IValidacionServicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppParcialesMauiTrapiella.Services
+{
+    public interface IValidacionServicio
+    {
+        bool IsValidEmail(string email);
+        bool IsValidPassword(string password);
+        bool IsValidRaza(string raza);
+        bool IsValidNombre(string nombre);
+        bool IsValid(string campo);
+    }
+}
