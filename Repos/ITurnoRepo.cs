98a5e294ed9bcb59f62e8f17153065abdc0feb5b
inaki <iakitrapiella27@hotmail.com>
2025-11-21 00:18:56 -0300

    agrego repositorios y sus interfaces para cada modelo (tablas)

---
+++ Repos/ITurnoRepo.cs
@@ -0,0 +1,19 @@
+﻿using AppParcialesMauiTrapiella.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppParcialesMauiTrapiella.Repos
+{
+    public interface ITurnoRepo
+    {
+        Task<List<Turno>> GetAllAsync();
+        Task<Turno> GetByIdAsync(int id);
+        Task<int> InsertAsync(Turno turno);
+        Task<int> UpdateAsync(Turno turno);
+        Task<int> DeleteAsync(Turno turno);
+        Task<List<Turno>> GetByMascotaIdAsync(int mascotaId);
+    }
+}
---
+++ Repos/IUsuarioRepo.cs
@@ -0,0 +1,16 @@
+﻿using AppParcialesMauiTrapiella.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppParcialesMauiTrapiella.Repos
+{
+    public interface IUsuarioRepo
+    {
+        Task<int> InsertAsync(Usuario usuario);
+        Task<Usuario> LoginAsync(string email, string password);
+        Task<Usuario> GetByMailAsync(string email);
+    }
+}
